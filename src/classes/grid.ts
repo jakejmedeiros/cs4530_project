d05a540ac3a39eda42065837308eea4d6fb783ec
psanpietro <sanpietro.pat@gmail.com>
2023-11-28 14:25:39 -0500

    Developed save and load for .csv. Theory Save should work fine. Load will definitely not work currently. Need to validate data usage for save and load before proceeding.

--- src/classes/grid.ts
+++ src/classes/grid.ts
@@ -1,5 +1,11 @@
+import * as fs from 'fs';
+import * as csv from 'fast-csv';
 import { Cells } from "./cellsImpl";
 import { ICells } from "src/interfaces/cells.interface";
+import { IRow } from "src/interfaces/row.interface";
+import { IColumn } from 'src/interfaces/column.interface';
+import { Column } from './column';
+import { Row } from './row';
 
 // Class for creating the grid of cells for the spreadsheet. Cells[x][y] where x is the row and y is the column
 // The Grid class is an implementation of the Singletone design pattern
@@ -69,4 +75,42 @@ export class Grid {
         })
         this.cells = initCells;
     }
+
+    //Save the entire grid into a .csv file using fast-csv
+    public saveToCSV(filePath: string): void {
+        const csvStream = csv.format({ headers: true });
+        const writableStream = fs.createWriteStream(filePath);
+    
+        csvStream.pipe(writableStream);
+    
+        this.cells.forEach((row) => {
+            const rowData = row.map(cell => cell.getValue().toString());
+            csvStream.write(rowData);
+        });
+    
+        csvStream.end();
+    }
+
+    
+    //Load from a .csv file using fast-csv
+    //the issue currently is that the construction of the Cells cannot tell what type the value is 
+    /*
+    public loadFromCSV(filePath: string): void {
+        const csvStream = fs.createReadStream(filePath)
+            .pipe(csv.parse({ headers: true }))
+            .on('data', (row) => {
+                const rowData: ICells[] = Object.values(row).map((value, index) => new Cells(value, 0, index));
+                this.addRow(rowData);
+            })
+            .on('end', () => {
+                console.log('CSV file successfully processed');
+            })
+            .on('error', (error) => {
+                console.error('Error parsing CSV file:', error);
+            });
+    }
+    
+    */
+    
+    
 }
